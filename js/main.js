12f5e71266eb2e279481a5086789ca3798342326
colombidesign <colombidesign@gmail.com>
2025-07-16 10:37:34 +0200

    Handle header fetch errors

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 // js/main.js
 
 fetch('./partials/header.html')
-  .then(res => res.text())
+  .then(res => {
+    if (!res.ok) throw new Error('Failed to fetch header');
+    return res.text();
+  })
   .then(html => {
-    document.getElementById('header-placeholder').innerHTML = html;
-  });
+    const placeholder = document.getElementById('header-placeholder');
+    if (placeholder) {
+      placeholder.innerHTML = html;
+    }
+  })
+  .catch(err => console.error('Header load error:', err));
